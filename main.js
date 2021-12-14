a4ff1e0ae85af873de786a0b31136387ba2493bf
Bilal Fawaz <bilal.fawaz@student.ehb.be>
2021-12-14 10:09:16 +0100

    Home pagina klaar

---
+++ main.js
@@ -0,0 +1,43 @@
+const baseUrl = "https://api.tvmaze.com/"
+document.querySelectorAll(".genre").forEach(e => e.onclick = filterByGenre)
+window.onload = setup
+
+async function setup() {
+    let shows = await getShows()
+    console.log(shows)
+    renderHomePage(shows[0])
+}
+
+async function filterByGenre(e) {
+    let genre = e.target.innerText
+    let shows = await getShows()
+    let filteredShows = shows.filter(s => s.genres.includes(genre))
+    if (filteredShows.length > 0) {
+        let showId = Math.floor(Math.random() * filteredShows.length)
+        renderHomePage(filteredShows[showId])
+    } else {
+        console.log("no shows found for this genre")
+    }
+}
+
+async function getShows() {
+    let response = await fetch(`${baseUrl}shows`)
+    let result = await response.json()
+    return result
+}
+
+function renderHomePage(show) {
+    let poster = document.getElementById("poster")
+    poster.src = show.image.original
+    setShowInfo(show)
+}
+
+function setShowInfo(show) {
+    let title = document.getElementById("title")
+    let description = document.getElementById("description")
+    let rating = document.getElementById("rating")
+
+    title.innerText = show.name
+    description.innerHTML = show.summary
+    rating.innerText = `${show.rating.average}/10`
+}
